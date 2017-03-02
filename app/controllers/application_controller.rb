7621cd9d1c6470095f4be1ebffb684bf2f70b359
Daniel Griffiths <logiconictvtube@gmail.com>
2017-03-01 23:18:49 -0500

    edited messages in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
 
   def hello
-    render html: "hello, world!"
+    render html: "Whaddup World!"
   end
 
   private
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     def logged_in_user
       unless logged_in?
   #store_location
-        flash[:danger] = "Please log in."
+        flash[:danger] = "Please Log In."
         redirect_to login_url
       end
     end
